f076bda3c65c2ab51937cb0249e08b31dd5eb9a7
Vitalii Shumakov <fafde8120@gmail.com>
2025-11-01 22:24:06 +0100

    tasks

---
+++ src/task-2.ts
@@ -0,0 +1,8 @@
+const product = {
+    id: 1,
+    title: "Tablet",
+    description: "Compact and fast",
+  };
+  
+  console.log(`Product: ${JSON.stringify(product)}`);
+  
---
+++ src/task-6.ts
@@ -0,0 +1,8 @@
+function getFirstElement(arr) {
+    return arr[0];
+  }
+  
+  getFirstElement([1, 2, 3]);           // 1
+  getFirstElement(["a", "b", "c"]);     // "a"
+  getFirstElement([true, false, true]); // true
+  
